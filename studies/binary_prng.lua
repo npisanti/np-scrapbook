870ea44e7749c7dbdffdfc8d370200c93d684d4b
Nicola Pisanti <nicola@npisanti.com>
2019-05-09 17:48:56 +0200

    updates studies

--- studies/binary_prng.lua
+++ studies/binary_prng.lua
@@ -7,38 +7,37 @@ resolution = 1
 
 ----------------------------------------------------
 function setup()
-    lvg.size( 300*resolution, 800*resolution )
+    us.size( 300*resolution, 800*resolution )
 end
 
 ----------------------------------------------------
 function draw()
-
+    us.relative()
+    us.translate( 0.0, -0.5 )
+        
     local step = 1.0 / (segments + 4.0)
 
-    lvg.background( 255, 255, 255, 255 )
-    
-    -- horizontally center 
-    lvg.translate( lvg.ratio()*0.5, 0.0 )
+    us.background( 255, 255, 255, 255 )
 
     -- draws the outline 
     
-    lvg.strokeWidth( 0.003 )
-    lvg.stroke( 0 )
-    lvg.begin()
+    us.strokeWidth( 0.003 )
+    us.stroke( 0 )
+    us.begin()
         for i=2, segments do           
             if i%2==0 then 
-                lvg.moveTo( 0.0, step*i )
+                us.moveTo( 0.0, step*i )
                 local nexti = i + 1.0 
-                lvg.lineTo(  0.0, step*nexti  )
+                us.lineTo(  0.0, step*nexti  )
             end
         end    
-    lvg.close()
+    us.close()
     
-    lvg.fill( 0 )
-    lvg.begin()
+    us.fill( 0 )
+    us.begin()
         local cradius = 0.005
-        lvg.circle( 0, (segments+2.)*step + cradius*0.5, cradius )
-    lvg.close()
+        us.circle( 0, (segments+2.)*step + cradius*0.5, cradius )
+    us.close()
 
 end
 
