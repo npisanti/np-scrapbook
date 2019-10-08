67b8476da207f595ca87383ec6a36c258fd20bed
Nicola Pisanti <nicola@npisanti.com>
2019-10-08 15:48:12 +0200

    mantras

--- sketches/mantra_01/01_image.lua
+++ sketches/mantra_01/01_image.lua
@@ -3,22 +3,29 @@ require "strict"
 
 local folder = "write_alpha"
 local count = 0
+local step;
 
 ----------------------------------------------------
 function setup()
+    
     png.load("/assets/ink/tests/"..folder..".png")
     px.framerate( 30 )
+
+    -- px.resize( 480, 480 )
 end
+
 
 ----------------------------------------------------
 function draw()
 
+    step = px.height() / 9
+    
     px.clear()
     px.color(255)
     
     png.select( folder )
     if lfo.clock(15) then 
-        png.draw( px.width()/2,  88 * count )  
+        png.draw( px.width()/2,  step * count )  
         
         count = count + 1 
         if count == 9 then
